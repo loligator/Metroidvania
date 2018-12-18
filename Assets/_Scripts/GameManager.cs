9d191e8610c14666a7101c26c147f32def8a51fa
loligator <loligator704@gmail.com>
2018-12-17 22:50:30 -0500

    Text and Debug Setup

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager GM;
+
+    private TextMeshProUGUI debugText;
+
+	// Use this for initialization
+	void Start () {
+        debugText = GameObject.FindGameObjectWithTag("Debug");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    static void Debug(string msg) {
+        debugText.text = 
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D _rb;
+
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0f) {
+            _rb.MovePosition(new Vector2(20f * Time.deltaTime, 0f));
+        }
+
+        if (Input.GetAxis("Horizontal") < 0f){
+            _rb.MovePosition(new Vector2(20f * Time.deltaTime, 0f));
+        }
+
+
+	}
+}
